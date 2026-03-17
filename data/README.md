5d3195b6577a38743c581bb9bcec9e24c914c531
willdaplum <49744586+willdaplum@users.noreply.github.com>
2026-03-17 01:08:47 -0500

    Simulates all march madness games

---
+++ data/README.md
@@ -0,0 +1,4 @@
+#### Directory for Training Data
+- Download csv files from [here](https://www.kaggle.com/competitions/march-machine-learning-mania-2026/data)
+- Those csv files go *here*, in this folder
+- Data can be added from elsewhere as well, but the script relies on MTeams.csv and MNCAATourneySeeds.csv from this source to build the bracket.
--- march-madness.py
+++ march-madness.py
@@ -1,44 +1,93 @@
-from helpers import Matchup, Team
 import numpy as np
 import pandas as pd
 
 
+SEED_LOCATIONS = {
+    1: 0,
+    16: 1,
+    8: 2,
+    9: 3,
+    5: 4,
+    12: 5,
+    4: 6,
+    13: 7,
+    6: 8,
+    11: 9,
+    3: 10,
+    14: 11,
+    7: 12,
+    10: 13,
+    2: 14,
+    15: 15,
+}
 
-def get_teams():
-    teams = []
+class Team:
+    def __init__(self, id, name, draw_location):
+        self.id = id
+        self.name = name
+        self.seed = int(draw_location[1:3])
+        self.draw_location = draw_location
+
+
+# final four:
+#   w -|
+#      + - |
+#   x -|   |
+#          + - champion
+#   y -|   |
+#      + - |
+#   z -|
+def create_bracket():
+    bracket_with_regions = {}
+    regions = "WXYZ"
+
+    for region in regions:
+        bracket_with_regions[region] = ["" for i in range(0, 16)]
+
     tournament_teams_df = pd.read_csv("data/MNCAATourneySeeds.csv")
     tournament_teams_df = tournament_teams_df.loc[tournament_teams_df["Season"] == 2026]
     teams_df = pd.read_csv("data/MTeams.csv")
     for team in tournament_teams_df.itertuples(index=False):
-        print(team)
         team_id = team[2]
         team_name = teams_df.loc[teams_df["TeamID"] == team_id]["TeamName"].squeeze()
         seed = team[1]
-        teams.append(Team(id=team_id, draw_location=seed, name=team_name))
+        bracket_with_regions[seed[0]][SEED_LOCATIONS[int(seed[1:3])]] = Team(
+            team_id, team_name, seed
+        )
+        
+    bracket = []
+    for region in regions:
+        bracket = bracket + bracket_with_regions[region]
+    return bracket
 
-    return teams
 
-
-def predict_matchup(matchup: Matchup):
+def predict_matchup(team1: Team, team2: Team):
     """Returns True if team 1 wins, false if team 2 wins"""
-    ###
-    # ADD TOURNAMENT PREDICTION LOGIC BELOW
-    
-    # e.g.: always predict the higher seeded team to win
-    if matchup.team1.seed <= matchup.team2.seed:
+    ### ADD TOURNAMENT PREDICTION LOGIC BELOW
+
+    # e.g.: always predict the higher (better) seeded team to win
+    if team1.seed <= team2.seed:
         return True
     return False
 
-    # END TOURNAMENT PREDICTION LOGIC
-    ###
+    ### END TOURNAMENT PREDICTION LOGIC
 
-def create_first_round_matchups():
-    regions = "WXYZ"
-    
 
 if __name__ == "__main__":
-    teams = get_teams()
-
+    bracket = create_bracket()
+    
     # simulate each game
-
-    # send to mm api endpoint
+    while len(bracket) > 1:
+        team1_wins = predict_matchup(bracket[0], bracket[1])
+        if(team1_wins):
+            bracket.append(bracket[0])
+            print("{} d. {}".format(bracket[0].name, bracket[1].name))
+        else:
+            bracket.append(bracket[1])
+            print("{} d. {}".format(bracket[0].name, bracket[1].name))
+        bracket = bracket[2:]
+            
+    print("Champion: {}".format(bracket[0].name))
+            
+        
+    # write results to json
